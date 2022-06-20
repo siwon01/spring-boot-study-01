1029fd6e382658399261686fa38b3ee9a311ab7d
Hongsiwon <82129658+Hongsiwon@users.noreply.github.com>
2022-06-20 15:53:45 +0900

    06/20 15:53 REFACTOR: PAGE 204

--- src/test/java/org/zerock/guestbook/GuestbookApplicationTests.java
+++ src/test/java/org/zerock/guestbook/GuestbookApplicationTests.java
@@ -1,13 +1,10 @@
 package org.zerock.guestbook;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GuestbookApplicationTests {
+public class GuestbookApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
 
 }
--- src/test/java/org/zerock/guestbook/repository/GuestbookRepositoryTests.java
+++ src/test/java/org/zerock/guestbook/repository/GuestbookRepositoryTests.java
@@ -1,17 +1,13 @@
 package org.zerock.guestbook.repository;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.zerock.guestbook.entity.Guestbook;
-
-import java.util.stream.IntStream;
 
 @SpringBootTest
 public class GuestbookRepositoryTests {
     @Autowired
     private GuestbookRepository guestbookRepository;
-
+    /*
     @Test
     public void insertDummies() {
 
@@ -25,4 +21,77 @@ public class GuestbookRepositoryTests {
             System.out.println(guestbookRepository.save(guestbook));
         });
     }
+    */
+
+    /*@Test
+    public void updateTest(){
+        Optional<Guestbook> result = guestbookRepository.findById(300L);
+        //존재하는 번호로 테스트
+
+        if(result.isPresent()){
+            Guestbook guestbook = result.get();
+
+            guestbook.changeTitle("Changed Title....");
+            guestbook.chanContent("Changed Content...");
+
+            guestbookRepository.save(guestbook);
+        }
+    }*/
+
+    /*
+        @Test
+        public void testQuery1(){
+
+            Pageable pageable = PageRequest.of(0, 10, Sort.by("gno").descending());
+            QGuestbook qGuestbook = QGuestbook.guestbook; //1
+
+            String keyword="1";
+
+            BooleanBuilder builder = new BooleanBuilder();  //2
+
+            BooleanExpression expression = qGuestbook.title.contains(keyword);  //3
+
+            builder.and(expression);    //4
+
+            Page<Guestbook> result = guestbookRepository.findAll(builder, pageable);    //5
+
+            result.stream().forEach(guestbook -> {
+                System.out.println(guestbook);
+            });
+        }
+        */
+
+/*
+
+    @Test
+    public void testQuery2(){
+
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("gno").descending());
+
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+
+        String keyword = "1";
+
+        BooleanBuilder builder = new BooleanBuilder();
+
+        BooleanExpression exTitle = qGuestbook.title.contains(keyword);
+
+        BooleanExpression exContent = qGuestbook.content.contains(keyword);
+
+        BooleanExpression exAll = exTitle.or(exContent);    // 1--------
+
+        //제목+내용+작성자 부분을 처리하고싶다면 동일하게 BooleanExpression을 생성하고 or조건으로 결합하면 됨
+
+        builder.and(exAll); //2-----------
+
+        builder.and(qGuestbook.gno.gt(0L)); //3-------
+
+        Page<Guestbook> result = guestbookRepository.findAll(builder, pageable);
+
+        result.stream().forEach(guestbook -> {
+            System.out.println(guestbook);
+        });
+
+    }
+*/
 }
